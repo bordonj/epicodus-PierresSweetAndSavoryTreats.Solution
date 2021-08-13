55b22ed3ca5ab5c95984fc3f3d786bffe356a9c3
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-13 10:41:36 -0700

    add initial files and folders

---
+++ Bakery/Models/Treat.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bakery.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.JoinEntities = new HashSet<FlavorTreat>();
+    }
+
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public string Ingredients { get; set; }
+    public string Price { get; set; }
+    public virtual ApplicationUser User { get; set; }
+
+    public virtual ICollection<FlavorTreat> JoinEntities { get;}
+  }
+}
