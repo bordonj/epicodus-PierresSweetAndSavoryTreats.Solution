96e8ecfdb5c6306a828d967d5580547bf03d0d70
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-13 12:06:45 -0700

    correct typo

--- Bakery/ViewModels/LoginViewModel.cs
+++ Bakery/ViewModels/LoginViewModel.cs
@@ -1,4 +1,4 @@
-namespace RecipeBox.ViewModels
+namespace Bakery.ViewModels
 {
     public class LoginViewModel
     {
--- Bakery/ViewModels/RegisterViewModel.cs
+++ Bakery/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace RecipeBox.ViewModels
+namespace Bakery.ViewModels
 {
     public class RegisterViewModel
     {
