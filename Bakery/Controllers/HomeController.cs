270d5b1a863214cb2c70aa73b0aa9ca640149b49
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-13 20:22:11 -0700

    add functionality to view both Flavors and Treats on splash page

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,13 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bakery.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly BakeryContext _db;
+      public HomeController(BakeryContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
+      List<Flavor> FlavorList = _db.Flavors.ToList();
+      List<Treat> TreatList = _db.Treats.ToList();
+      ViewBag.Flavors = FlavorList;
+      ViewBag.Treats = TreatList;
         return View();
       }
 
