744a1435bcc3c08e39875b0fc164d9b782865f2c
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-13 20:23:48 -0700

    add migration for description property

---
+++ Bakery/Migrations/20210814022602_DescriptionProp.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bakery.Migrations
+{
+    public partial class DescriptionProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Treats",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Flavors",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Treats");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Flavors");
+        }
+    }
+}
